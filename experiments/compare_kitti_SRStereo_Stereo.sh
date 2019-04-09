3b7ba587c284c280aa70dc5980fd7dbd58db36cb
Cheng Zhang <850576570@qq.com>
2019-04-09 09:51:21 +0800

    Renamed experiments.

---
+++ experiments/compare_kitti_SRStereo_Stereo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+. ./settings.sh
+
+# GPU settings
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+nGPUs=$(( (${#CUDA_VISIBLE_DEVICES} + 1) / 2 ))
+
+## Stereo
+# create baseline PSMNet
+PYTHONPATH=./ python $stereo_train --model PSMNet --outputFolder experiments/compare_kitti_SRStereo_Stereo --dispScale 1 --dataPath $kitti2015_dataset --dataset kitti2015 --chkpoint $pretrained_PSMNet_sceneflow --batchSize 12 $nGPUs --trainCrop 256 512 --evalFcn outlier --epochs 1200 --lr 0.001 200 0.0001 --logEvery 10 --testEvery 10 --saveEvery 100
