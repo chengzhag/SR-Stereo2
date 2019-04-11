e04fd71097839812a8aa7d4fd77509da2392c950
Cheng Zhang <850576570@qq.com>
2019-04-11 14:50:20 +0800

    Fixed problem with several experiments.

--- experiments/compare_carla_SR_SRdisp.sh
+++ experiments/compare_carla_SR_SRdisp.sh
@@ -2,12 +2,12 @@
 . ./settings.sh
 
 # GPU settings
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 nGPUs=$(( (${#CUDA_VISIBLE_DEVICES} + 1) / 2 ))
 
 # finetune SRdisp
-PYTHONPATH=./ python train/SR_train.py --model EDSRdisp --outputFolder experiments/compare_carla_SR_SRdisp --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_EDSR_DIV2K --batchSize 4 $(( 2 * $nGPUs)) --trainCrop 96 1360 --evalFcn l1 --epochs 20 --lr 0.0001 10 0.00005 15 0.00002 --logEvery 50 --testEvery 2 --saveEvery 20 --validSetSample 1 --half
+PYTHONPATH=./ python train/SR_train.py --model EDSRdisp --outputFolder experiments/compare_carla_SR_SRdisp --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_EDSR_DIV2K --batchSize 16 $(( 2 * $nGPUs)) --trainCrop 64 2040 --evalFcn l1 --epochs 40 --lr 0.0005 15 0.0002 20 0.0001 25 0.00005 30 0.00002 35 0.00001 --logEvery 50 --testEvery 5 --saveEvery 40 --validSetSample 1 --half
 
 # finetune SR
-PYTHONPATH=./ python train/SR_train.py --model EDSR --outputFolder experiments/compare_carla_SR_SRdisp --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_EDSR_DIV2K --batchSize 4 $(( 2 * $nGPUs)) --trainCrop 96 1360 --evalFcn l1 --epochs 20 --lr 0.0001 10 0.00005 15 0.00002 --logEvery 50 --testEvery 2 --saveEvery 20 --validSetSample 1 --half
+PYTHONPATH=./ python train/SR_train.py --model EDSR --outputFolder experiments/compare_carla_SR_SRdisp --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_EDSR_DIV2K --batchSize 16 $(( 2 * $nGPUs)) --trainCrop 64 2040 --evalFcn l1 --epochs 40 --lr 0.0001 25 0.00005 30 0.00002 35 0.00001 --logEvery 50 --testEvery 5 --saveEvery 40 --validSetSample 1 --half
 
--- experiments/compare_kitti_SRStereo_Stereo.sh
+++ experiments/compare_kitti_SRStereo_Stereo.sh
@@ -7,4 +7,7 @@ nGPUs=$(( (${#CUDA_VISIBLE_DEVICES} + 1) / 2 ))
 
 ## Stereo
 # create baseline PSMNet
-PYTHONPATH=./ python $stereo_train --model PSMNet --outputFolder experiments/compare_kitti_SRStereo_Stereo --dispScale 1 --dataPath $kitti2015_dataset --dataset kitti2015 --chkpoint $pretrained_PSMNet_sceneflow --batchSize 12 $nGPUs --trainCrop 256 512 --evalFcn outlier --epochs 1200 --lr 0.001 200 0.0001 --logEvery 10 --testEvery 10 --saveEvery 100
+PYTHONPATH=./ python train/Stereo_train.py --model PSMNet --outputFolder experiments/compare_kitti_SRStereo_Stereo --dispScale 1 --dataPath $kitti2015_dataset --dataset kitti2015 --chkpoint $pretrained_PSMNet_sceneflow --batchSize 12 $nGPUs --trainCrop 256 512 --evalFcn outlier --epochs 1200 --lr 0.001 200 0.0001 --logEvery 50 --testEvery 10 --saveEvery 100
+
+## SRStereo
+# TODO
