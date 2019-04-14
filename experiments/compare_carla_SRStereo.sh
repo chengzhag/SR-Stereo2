2363be0d3e0ed2a40fca56f2dd9712622b66dd8d
Cheng Zhang <850576570@qq.com>
2019-04-14 16:57:54 +0800

    Added task queue.

---
+++ experiments/compare_carla_SRStereo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+. ./settings.sh
+
+# GPU settings
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+nGPUs=$(( (${#CUDA_VISIBLE_DEVICES} + 1) / 2 ))
+
+# SRStereo
+# train EDSRbaselinePSMNetDown
+PYTHONPATH=./ python train/Stereo_train.py --model SRStereo EDSR PSMNet --outputFolder experiments/compare_carla_SRStereo --dispScale 2 --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_carla_EDSRbaseline $pretrained_carla_PSMNetDown --loadScale 1 0.5 --batchSize 12 $nGPUs --trainCrop 128 1024 --evalFcn l1 --epochs 5 --lr 0.0001 --lossWeights 0.5 0.375 0.125 --logEvery 50 --testEvery -5 --saveEvery 5 --half
+#PYTHONPATH=./ python train/Stereo_train.py --model SRStereo EDSR PSMNet --outputFolder experiments/compare_carla_SRStereo --dispScale 2 --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_carla_EDSRbaseline $pretrained_carla_PSMNetDown --loadScale 1 0.5 --batchSize 4 $nGPUs --trainCrop 256 1536 --evalFcn l1 --epochs 5 --lr 0.0001 --lossWeights 0.5 0.375 0.125 --logEvery 50 --testEvery -5 --saveEvery 5 --half
+
+
--- experiments/compare_carla_Stereo_StereoDown.sh
+++ experiments/compare_carla_Stereo_StereoDown.sh
@@ -9,7 +9,6 @@ nGPUs=$(( (${#CUDA_VISIBLE_DEVICES} + 1) / 2 ))
 # train PSMNetDown
 PYTHONPATH=./ python train/Stereo_train.py --model StereoDown PSMNet --outputFolder experiments/compare_carla_Stereo_StereoDown --dispScale 2 --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_carla_PSMNetDown --loadScale 1 0.5 --batchSize 12 $nGPUs --trainCrop 128 1024 --evalFcn l1 --epochs 5 --lr 0.0001 --lossWeights 0.75 0.25 --logEvery 50 --testEvery -5 --saveEvery 5 --half
 
-
 # Stereo
 # train PSMNet
 PYTHONPATH=./ python train/Stereo_train.py --model PSMNet --outputFolder experiments/compare_carla_Stereo_StereoDown --dispScale 1 --dataPath $carla_dataset --dataset carla --chkpoint $pretrained_carla_PSMNet --loadScale 0.5 --batchSize 12 $nGPUs --trainCrop 128 1024 --evalFcn l1 --epochs 5 --lr 0.0001 --logEvery 50 --testEvery -5 --saveEvery 5 --half
