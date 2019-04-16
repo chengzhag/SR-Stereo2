5c413f8e645e0f7962e95b321a02b9e1f3059afa
Cheng Zhang <850576570@qq.com>
2019-04-16 11:14:34 +0800

    Added PSMNet half precision full support. Added cropsize compare experiment for StereoDown.

--- models/Stereo/RawPSMNet/stackhourglass.py
+++ models/Stereo/RawPSMNet/stackhourglass.py
@@ -113,10 +113,8 @@ class PSMNet(nn.Module):
         targetimg_fea = self.feature_extraction(right)
 
         # matching
-        cost = Variable(
-            torch.FloatTensor(refimg_fea.size()[0], refimg_fea.size()[1] * 2, self.maxdisp // 4, refimg_fea.size()[2],
-                              refimg_fea.size()[3]).zero_()).cuda()
-
+        cost = torch.zeros(refimg_fea.size()[0], refimg_fea.size()[1] * 2, self.maxdisp // 4, refimg_fea.size()[2],
+                              refimg_fea.size()[3]).type_as(left).cuda()
         for i in range(self.maxdisp // 4):
             if i > 0:
                 cost[:, :refimg_fea.size()[1], i, :, (i * self.dispScale):] = refimg_fea[:, :, :, (i * self.dispScale):]
@@ -147,16 +145,16 @@ class PSMNet(nn.Module):
             cost2 = F.interpolate(cost2, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear',align_corners=False)
 
             cost1 = torch.squeeze(cost1, 1)
-            pred1 = F.softmax(cost1, dim=1)
+            pred1 = F.softmax(cost1, dim=1, dtype=left.dtype)
             pred1 = disparityregression(self.maxdisp)(pred1)
 
             cost2 = torch.squeeze(cost2, 1)
-            pred2 = F.softmax(cost2, dim=1)
+            pred2 = F.softmax(cost2, dim=1, dtype=left.dtype)
             pred2 = disparityregression(self.maxdisp)(pred2)
 
         cost3 = F.interpolate(cost3, [self.maxdisp, left.size()[2], left.size()[3]], mode='trilinear',align_corners=False)
         cost3 = torch.squeeze(cost3, 1)
-        pred3 = F.softmax(cost3, dim=1)
+        pred3 = F.softmax(cost3, dim=1, dtype=left.dtype)
         pred3 = disparityregression(self.maxdisp)(pred3)
 
         if self.training:
