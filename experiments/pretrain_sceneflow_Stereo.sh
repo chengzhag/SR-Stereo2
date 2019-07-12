061bd508b1be2dd85e10b81be3bcde363b7ac07d
Cheng Zhang <850576570@qq.com>
2019-07-12 10:50:29 +0800

    Added experiment compare_kitti_FeatureStereo.

--- experiments/pretrain_sceneflow_Stereo.sh
+++ experiments/pretrain_sceneflow_Stereo.sh
@@ -21,18 +21,24 @@ nGPUs=$(( (${#CUDA_VISIBLE_DEVICES} + 1) / 2 ))
 # train GANet
 #PYTHONPATH=./ python train/Stereo_train.py --model GANet --outputFolder experiments/pretrain_sceneflow_Stereo --dispScale 1 --dataPath $sceneflow_dataset --dataset sceneflow --loadScale 1 --batchSize 4 $nGPUs --trainCrop 240 576 --evalFcn l1 --epochs 10 --lr 0.001 --logEvery 50 --testEvery 10 --saveEvery 1 --mask 1 1 1 0
 
+## FeatureStereo
+
 # train MDSRfeaturePSMNetBody
 #PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo MDSRfeature PSMNetBody --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint $pretrained_DIV2K_MDSR --loadScale 1 --batchSize 4 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.001 --lossWeights 0 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0 --noComet
 
 # train EDSRfeaturePSMNetBody
-# only update body (SERVER 95)
+# only update body
 #PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNetBody --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint $pretrained_DIV2K_EDSR_baseline_x2 None --loadScale 1 --batchSize 12 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.001 --lossWeights 0 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0
-# update feature and body from epoch 5 (SERVER 162)
+# update feature and body from epoch 5
 #PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNetBody --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint ${experiment_dir}/pretrain_sceneflow_Stereo/Stereo_train/190704112543_model_FeatureStereo_EDSRfeature_PSMNetBody_loadScale_1.0_trainCrop_256_512_batchSize_12_4_lossWeights_0.0_1.0_sceneflow/checkpoint_epoch_0005_it_02955.tar --loadScale 1 --batchSize 4 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.0001 --lossWeights 1 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0 --resume toNew
-# update feature and body from begining (SERVER 162)
+# update feature and body from begining
 #PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNetBody --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint $pretrained_DIV2K_EDSR_baseline_x2 None --loadScale 1 --batchSize 4 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.0001 --lossWeights 1 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0
+# update feature and body from begining wihout loading SRfeature
+#PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNetBody --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --loadScale 1 --batchSize 4 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.0001 --lossWeights 1 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0
 
 # train EDSRfeaturePSMNet
-# only update body (SERVER 95)
-PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNet --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint $pretrained_DIV2K_EDSR_baseline_x2 None --loadScale 1 --batchSize 8 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.001 --lossWeights 0 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0
+# only update body
+#PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNet --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint $pretrained_DIV2K_EDSR_baseline_x2 None --loadScale 1 --batchSize 8 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.001 --lossWeights 0 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0
+# update feature and body
+#PYTHONPATH=./ python train/Stereo_train.py --model FeatureStereo EDSRfeature PSMNet --outputFolder experiments/pretrain_sceneflow_Stereo --dataPath $sceneflow_dataset --dataset sceneflow --chkpoint $pretrained_DIV2K_EDSR_baseline_x2 None --loadScale 1 --batchSize 4 $nGPUs --trainCrop 256 512 --evalFcn l1 --epochs 10 --lr 0.0001 --lossWeights 1 1 --logEvery 50 --testEvery 1 --saveEvery 1 --mask 1 1 1 0
 
